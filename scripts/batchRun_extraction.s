28212a6ed849db3ee3f6d21ce70b7227c7c4bf5b
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2018-11-05 00:29:24 -0500

    batch code to run extractions

--- scripts/batchRun_extraction.s
+++ scripts/batchRun_extraction.s
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=50GB
-#SBATCH --job-name=counterfactual_extraction
+#SBATCH --job-name=extraction
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nj995@nyu.edu
 #SBATCH --output=slurm_%j.out
@@ -12,4 +12,5 @@
 module purge
 module load python3/intel/3.6.3
 
+jupyter nbconvert --inplace --ExecutePreprocessor.timeout=-1 --execute inca_extraction.ipynb
 jupyter nbconvert --inplace --ExecutePreprocessor.timeout=-1 --execute counterfactual_extraction.ipynb
